ff952e1502f674e80de7fb3b5c7ef3c5f0ad985f
Victoran0 <victoranosunkvic@gmail.com>
2024-11-12 23:24:06 +0100

    Clerk provider working fine

---
+++ src/playground.ts
@@ -0,0 +1,10 @@
+import { db } from "./server/db";
+
+await db.user.create({
+    data: {
+        emailAddress: 'test@mail.com',
+        firstName: 'Elliorr',
+        lastName: 'Boyrano',
+    }
+})
+console.log("done")
