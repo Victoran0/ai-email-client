233ddb46bbdb35b640b7de10f38023e2d2e86262
Victoran0 <victoranosunkvic@gmail.com>
2024-11-19 04:34:11 +0100

    Select email from dropdown and save value using useLocalStorage of "usehooks-ts"

--- src/app/mail/account-switcher.tsx
+++ src/app/mail/account-switcher.tsx
@@ -1,6 +1,9 @@
 'use client'
 import { api } from '@/trpc/react'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import React from 'react'
+import {useLocalStorage} from 'usehooks-ts'
+import { cn } from '@/lib/utils'
 
 type Props = {
     isCollapsed: boolean 
@@ -8,12 +11,38 @@ type Props = {
 
 const AccountSwitcher = ({isCollapsed}: Props) => {
     const {data} = api.account.getAccounts.useQuery()
-    
+    const [accountId, setAccountId] = useLocalStorage('accountId', "")
+
+    if (!data) return null
     return (
-        <div>
-            {data?.map((account, id) => {
-                return <div key={id}>{account.emailAddress}</div>
-            })}
+        <div className="items-center gap-2 flex w-full">
+        <Select defaultValue={accountId} onValueChange={setAccountId}>
+            <SelectTrigger
+                className={cn(
+            "flex w-full flex-1 items-center gap-2 [&>span]:line-clamp-1 [&>span]:flex [&>span]:w-full [&>span]:items-center [&>span]:gap-1 [&>span]:truncate [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0",
+            isCollapsed &&
+            "flex h-9 w-9 shrink-0 items-center justify-center p-0 [&>span]:w-auto [&>svg]:hidden"
+          )}
+            >
+                <SelectValue placeholder="Select an account">
+                    <span className={cn({ "hidden": !isCollapsed })}>
+                        {data.find(account => account.id === accountId)?.emailAddress[0]}
+                    </span>
+                    <span className={cn({ "hidden": isCollapsed, 'ml-2': true })}>
+                        {data.find(account => account.id === accountId)?.emailAddress}
+                    </span>
+                </SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+                {data.map((account) => {
+                    return (
+                        <SelectItem key={account.id} value={account.id}>
+                            {account.emailAddress}
+                        </SelectItem>
+                    )
+                })}
+            </SelectContent>
+        </Select>
         </div>
     )
 }
