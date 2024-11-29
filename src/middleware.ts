42dd12732001fffd37bdb8d5ac9baf9823068564
Victoran0 <victoranosunkvic@gmail.com>
2024-11-29 01:04:43 +0100

    Added a more aesthetic Home Page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/clerk/webhook(.*)', '/api/initial-sync(.*)', '/api/stripe/webhook(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/clerk/webhook(.*)', '/api/initial-sync(.*)', '/api/stripe/webhook(.*)', '/'])
 
 // If a user try to access a page and it is not a public route, the user will be required to login
 // We can also add other routes to the list and make then public
